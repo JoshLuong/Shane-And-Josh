642252bbd1778ea67bb4ca705495769b72b1e77a
Josh Luong <joshualuong@hotmail.com>
2022-07-13 23:02:42 -0700

    add pic and meta

--- shane-and-josh/src/components/Image.js
+++ shane-and-josh/src/components/Image.js
@@ -1,12 +1,26 @@
 import "../App.css";
+import exifr from "exifr/dist/full.esm.mjs";
+import React, { useState } from "react";
 
 function Image(props) {
+  const [metaData, setMetaData] = useState("");
+  React.useEffect(() => {
+    exifr.parse(props.src).then((output) => {
+      setMetaData(output);
+    });
+  }, []);
   return (
-    <div style={{position: 'relative'}}>
-    <div className="hover-div">
-      {props.meta}
-    </div>
-    <img height="100%" width='100%' src={props.src}></img>
+    <div style={{ position: "relative" }}>
+      <div className="hover-div">
+        <div>{props.meta}</div>
+        <div className="meta-text">{metaData.Model}</div>
+        <div className="meta-text">
+          {metaData?.Make === "Canon"
+            ? metaData?.LensModel
+            : metaData?.CreateDate?.toDateString()}
+        </div>
+      </div>
+      <img height="100%" width="100%" src={props.src}></img>
     </div>
   );
 }
